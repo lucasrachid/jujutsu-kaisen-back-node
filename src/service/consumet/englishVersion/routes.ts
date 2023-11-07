97991a09c7b7fce51836aa53cabb6e2af4eac504
Lucas Rachid Martins <lucasrachid@hotmail.com>
2023-11-07 02:07:22 -0300

    pequena correcao tipagem.

--- src/service/consumet/englishVersion/routes.ts
+++ src/service/consumet/englishVersion/routes.ts
@@ -3,15 +3,15 @@ import express from 'express';
 
 const routesEN = express.Router();
 
-routesEN.get('/englishVersion/mangaSearch/:mangaName', async (req: any, res: any) => {
+routesEN.get('/englishVersion/mangaSearch/:mangaName', async (req, res) => {
   res.send(await searchForManga(req.params.mangaName));
 });
 
-routesEN.get('/englishVersion/mangaInfo/:mangaId', async (req: any, res: any) => {
+routesEN.get('/englishVersion/mangaInfo/:mangaId', async (req, res) => {
   res.send(await getMangaInfo(req.params.mangaId));
 });
 
-routesEN.get('/englishVersion/mangaChapter/:mangaChapter', async (req: any, res: any) => {
+routesEN.get('/englishVersion/mangaChapter/:mangaChapter', async (req, res) => {
   res.send(await getMangaChapter(req.params.mangaChapter));
 });
 
